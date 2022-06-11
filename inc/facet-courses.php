a350dfcc859c2a5b01fb58c47cdd3d2fe0f5faf4
woodwardtw <twwoodward@me.com>
2022-06-11 16:10:27 -0400

    fix courses

--- inc/facet-courses.php
+++ inc/facet-courses.php
@@ -9,8 +9,8 @@
 defined( 'ABSPATH' ) || exit;
 
 while ( have_posts() ): the_post(); ?>
-    <div class="col-md-6">
-        <div class="card h-100">         
+    <div class="col-md-6 d-flex">
+        <div class="card">         
           <div class="card-body">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>        
           </div>
